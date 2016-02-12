b51c77bb13950db600f2062e794f0bf22285a7a6
张帆@Home <zhangfan@dajiagame.com>
2016-02-12 15:35:15 +0800

    首次提交

---
+++ Assets/Shadow/Scripts/Edge.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace zephyr.twodshadow
+{
+    /// <summary>
+    /// 光照边缘的存储类
+    /// </summary>
+    public class Edge
+    {
+        /// <summary>
+        /// 起始、中点与结束点
+        /// </summary>
+        public Vector2 PointStart, PointMiddle, PointEnd;
+
+        /// <summary>
+        /// 前一个与后一个边缘的ID
+        /// </summary>
+        public int Prev, Next;
+
+        /// <summary>
+        /// 与光源的距离
+        /// </summary>
+        public float Distance;
+
+        /// <summary>
+        /// 基于起始点、终点和进行构造
+        /// warning: 构造完毕后Distance,Prev与Next并没有赋值
+        /// </summary>
+        /// <param name="pointStart"></param>
+        /// <param name="pointEnd"></param>
+        /// <param name="projecter"></param>
+        public Edge(Vector2 pointStart, Vector2 pointEnd)
+        {
+            PointStart = pointStart;
+            PointEnd = pointEnd;
+            PointMiddle = new Vector2(pointStart.x + pointEnd.x, pointEnd.y + pointEnd.y)/2;
+        }
+    }
+}
---
+++ Assets/Shadow/Scripts/ShadowManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Completed;
+
+namespace zephyr.twodshadow
+{
+
+    public class ShadowManager
+    {
+        /// <summary>
+        /// 地图中的所有边缘
+        /// </summary>
+        private Dictionary<int, Edge> _dicEdges;
+        /// <summary>
+        /// 被照亮的边缘
+        /// </summary>
+        private Dictionary<int, Edge> _dicLightedEdges; 
+
+        public void Init(BoardManager boardManager)
+        {
+            GameObject[] wallTiles = boardManager.wallTiles;
+            _dicEdges = new Dictionary<int, Edge>();
+            for (int i = 0; i < wallTiles.Length; i++)
+            {
+                InitAllEdges(wallTiles[i]);
+            }
+        }
+
+        /// <summary>
+        /// 传入tile块构造edge数据
+        /// 假定：tile都为方形，且居中绘制
+        /// </summary>
+        /// <param name="tileObject"></param>
+        private void InitAllEdges(GameObject tile)
+        {
+            //1.构造出4个边，各自中点
+            Sprite sprite = tile.GetComponent<SpriteRenderer>().sprite;
+            Debug.Log(sprite);
+        }
+    }
+}
